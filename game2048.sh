f55559bdda6c687242a7df8038831abb96966c3a
V Play Games <vaibhavnargwani28@gmail.com>
2025-06-21 15:39:30 +0530

    Change cell structure

--- game2048.sh
+++ game2048.sh
@@ -29,17 +29,21 @@ move_rc=(
 # 1 -> 0
 # 2 -> +1
 
-# Cell structure: iiv
-# i -> row
-# i -> column
-# v -> value
-
 cell_value_formatted=("    " "  2 " "  4 " "  8 " " 16 " " 32 " " 64 " "128 " "256 " "512 " "1024" "2048")
 spawns=(1 1 1 1 2)
 win=0
 score=0
 modifiedLastMove=0
-board=()
+cellValues=()
+cellModified=()
+
+set_modified() {
+  cellModified[$(get_index $1 $2)]=$3
+}
+
+get_modified() {
+  echo ${cellModified[$(get_index $1 $2)]}
+}
 
 end() {
   echo "Thanks for playing!"
@@ -68,7 +72,8 @@ game2048() {
   echo "You chose a board of size $size x $size"
   for ((i = 0; i < size; i++)); do
     for ((j = 0; j < size; j++)); do
-      set_cell i j "$i$j$((0))$((0))$((0))"
+      set_value i j 0
+      set_modified i j 0
     done
   done
 
@@ -86,7 +91,7 @@ spawn() {
   local empty_cells=()
   for ((i = 0; i < size; i++)); do
     for ((j = 0; j < size; j++)); do
-      if (( $(get_cell_value "$i" "$j") == 0 )); then
+      if (( $(get_value $i $j) == 0 )); then
         empty_cells+=("$i $j")
       fi
     done
@@ -96,7 +101,7 @@ spawn() {
   fi
   local random_index=$((RANDOM % ${#empty_cells[@]}))
   # shellcheck disable=SC2086
-  update_cell_value ${empty_cells[$random_index]} "$value"
+  set_value ${empty_cells[$random_index]} $value
 }
 
 get_index() {
@@ -105,45 +110,18 @@ get_index() {
   echo $((row * size + col))
 }
 
-set_cell() {
-  local row=$1
-  local col=$2
-  local value=$3
-  board[$(get_index "$row" "$col")]="$value"
+set_value() {
+  cellValues[$(get_index $1 $2)]=$3
 }
 
-get_cell() {
+get_value() {
   local row=$1
   local col=$2
-  echo "${board[$(get_index "$row" "$col")]}"
-}
-
-get_cell_value() {
-  local row=$1
-  local col=$2
-
-  local cell_data
-  cell_data=$(get_cell "$row" "$col")
-
-  local value=${cell_data:2:1}
-  echo "$value"
-}
-
-update_cell_value() {
-  local row=$1
-  local col=$2
-  local new_value=$3
-
-  local cell_data
-  cell_data=$(get_cell "$row" "$col")
-
-  local i=${cell_data:0:1}
-  local j=${cell_data:1:1}
-
-  local moved=${cell_data:3:1}
-  local modified=${cell_data:4:1}
-
-  set_cell "$row" "$col" "$i$j$new_value$moved$modified"
+  if (( row < 0 || row >= size || col < 0 || col >= size )); then
+    echo -1
+  else
+    echo ${cellValues[$(get_index $row $col)]}
+  fi
 }
 
 display_board() {
@@ -152,9 +130,7 @@ display_board() {
   for ((i = 0; i < size; i++)); do
     echo -n "| "
     for ((j = 0; j < size; j++)); do
-      local value
-      value=$(get_cell_value $i $j)
-      echo -n "${cell_value_formatted[$value]} | "
+      echo -n "${cell_value_formatted[$(get_value $i $j)]} | "
     done
     echo -e ""
   done
@@ -176,22 +152,22 @@ right() {
 
 move() {
   local move=$1
-  local moveRC="${move_rc[$move]}"
+  local moveRC=${move_rc[$move]}
   # var row = move.row * (size - 1)
   # var column = move.column * (size - 1)
   local row=$(((moveRC / 10) * (size - 1)))
   local col=$(((moveRC % 10) * (size - 1)))
 
   local moveRCDelta="${move_rc_changes[$move]}"
-  local rowDel=$(absolute $((moveRCDelta / 10 % 2)))
-  local colDel=$(absolute $((moveRCDelta % 10 % 2)))
+  local rowDel=$(absolute $(get_row_delta $move))
+  local colDel=$(absolute $(get_col_delta $move))
   # repeat(size) {
   #     cells[row][column].move(move)
   #     row += move.columnChange.absoluteValue
   #     column += move.rowChange.absoluteValue
   # }
   for ((i = 0; i < size; i++)); do
-    move_cell "$row" "$col" "$move"
+    move_cell $row $col $move
     row=$((row + rowDel))
     col=$((col + colDel))
   done
@@ -219,8 +195,20 @@ absolute() {
   if (( value < 0 )); then
     echo $(( -value ))
   else
-    echo "$value"
+    echo $value
   fi
+}
+
+get_row_delta() {
+  local move=$1
+  local moveRCDelta=${move_rc_changes[$move]}
+  echo $((moveRCDelta / 10 % 2))
+}
+
+get_col_delta() {
+  local move=$1
+  local moveRCDelta=${move_rc_changes[$move]}
+  echo $((moveRCDelta % 10 % 2))
 }
 
 move_cell() {
