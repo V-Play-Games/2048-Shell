7bc10ec1be747f038ba48c14fdf7f4a7882a8cd8
V Play Games <vaibhavnargwani28@gmail.com>
2025-06-21 14:26:38 +0530

    Add move logic

--- game2048.sh
+++ game2048.sh
@@ -18,26 +18,33 @@ move_rc_changes=(
   10 # Left
   12 # Right
 )
+
+move_rc=(
+  00 # Up
+  10 # Down
+  00 # Left
+  01 # Right
+)
 # 0 -> -1
 # 1 -> 0
 # 2 -> +1
 
-# Cell structure: iivMm
+# Cell structure: iiv
 # i -> row
 # i -> column
 # v -> value
-# M -> moved (0 for no, 1 for yes)
-# m -> modified (0 for no, 1 for yes)
 
 cell_value_formatted=("    " "  2 " "  4 " "  8 " " 16 " " 32 " " 64 " "128 " "256 " "512 " "1024" "2048")
 spawns=(1 1 1 1 2)
 win=0
 lose=0
 score=0
+modifiedLastMove=0
 board=()
 
 end() {
   echo "Thanks for playing!"
+  exit 0
 }
 
 help() {
@@ -80,12 +87,12 @@ spawn() {
   local empty_cells=()
   for ((i = 0; i < size; i++)); do
     for ((j = 0; j < size; j++)); do
-      if [[ $(get_cell_value "$i" "$j") == 0 ]]; then
+      if (( $(get_cell_value "$i" "$j") == 0 )); then
         empty_cells+=("$i $j")
       fi
     done
   done
-  if [[ ${#empty_cells[@]} -eq 0 ]]; then
+  if (( ${#empty_cells[@]} == 0 )); then
       return
   fi
   local random_index=$((RANDOM % ${#empty_cells[@]}))
@@ -169,19 +176,67 @@ right() {
 }
 
 move() {
-  local move_direction=$1
-  echo "Moving ${move_direction}..."
+  local move=$1
+  local moveRC="${move_rc[$move]}"
+  # var row = move.row * (size - 1)
+  # var column = move.column * (size - 1)
+  local row=$(((moveRC / 10) * (size - 1)))
+  local col=$(((moveRC % 10) * (size - 1)))
+
+  local moveRCDelta="${move_rc_changes[$move]}"
+  local rowDel=$(absolute $((moveRCDelta / 10 % 2)))
+  local colDel=$(absolute $((moveRCDelta % 10 % 2)))
+  # repeat(size) {
+  #     cells[row][column].move(move)
+  #     row += move.columnChange.absoluteValue
+  #     column += move.rowChange.absoluteValue
+  # }
+  for ((i = 0; i < size; i++)); do
+    move_cell "$row" "$col" "$move"
+    row=$((row + rowDel))
+    col=$((col + colDel))
+  done
+  # if (anyMove) {
+  #     spawn()
+  # }
+  if ((modifiedLastMove == 1)); then
+    spawn
+  fi
+  # lose = flatCells.none { it.canMove() }
+  check_lose
   display_board
   echo "Score: $score"
-  if [[ $win == 1 ]]; then
+  if (( win == 1 )); then
     echo "You win!"
     end
   fi
-  if [[ $lose == 1 ]]; then
+  if (( lose == 1 )); then
     echo "You lose!"
     end
   fi
   menu
+}
+
+absolute() {
+  local value=$1
+  if (( value < 0 )); then
+    echo $(( -value ))
+  else
+    echo "$value"
+  fi
+}
+
+move_cell() {
+  local row=$1
+  local col=$2
+  local move=$3
+
+  echo "Moving cell at ($row, $col) in direction $move"
+}
+
+check_lose() {
+  # lose = flatCells.none { it.canMove() }
+  lose=0
 }
 
 game2048
